50fdf0a89accb0ec0049bd587d6f70a31ff02dde
gastonmartres <31313527+gastonmartres@users.noreply.github.com>
2024-09-20 12:24:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def return_status_code(code):
         abort(400, description="Invalid HTTP status code")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
