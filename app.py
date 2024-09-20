0de9227c00ec40cf2358139f98d5e00ceb413dc7
gastonmartres <31313527+gastonmartres@users.noreply.github.com>
2024-09-20 12:23:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
+# Ruta principal que describe cómo utilizar la app
+@app.route('/')
+def index():
+    return jsonify({
+        "message": "Bienvenido a HTTP Status Code Emulator!",
+        "usage": "Para usar este servicio, enviar un request GET a '/status/<code>',  donde <code> es un codigo HTTP válido. (e.j., /status/200, /status/404)."
+    })
+
+# Ruta que devuelve el código HTTP solicitado
 @app.route('/status/<int:code>', methods=['GET'])
 def return_status_code(code):
     # Verificar si es un código HTTP válido
@@ -11,4 +20,4 @@ def return_status_code(code):
         abort(400, description="Invalid HTTP status code")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
